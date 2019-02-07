2038ca2c726e6c1b970c1fa13ef30975edfe0576
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-02-06 16:33:50 -0800

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+	const embed = new Discord.RichEmbed()
+    .setColor(16754843)
+    .setTitle('fk off');
+
+  message.channel.send(embed);
+	
+}
