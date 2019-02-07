63a4cae8cdf58a71abb938aaced99dd73c45f4bd
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-02-06 18:06:21 -0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ exports.run =  (client, message, args, ops) => {
   const embed = new Discord.RichEmbed()
     .setColor(16754843)
     //.setDescription('don\'t ask me for help')
-    .setDescription('+ping\n+roles\n+ascii\t[message]\n+poll\t[question]\n+scrabble\t[word]\n+wwf\t[word]\n+calc\t[equation]')
+    .setDescription('+ping\n+roles\n+poll\t[question]\n+scrabble\t[word]\n+wwf\t[word]')
     .setAuthor('Nori Bot Commands', "https://imgur.com/nDAhzzg.png")
 
   message.channel.send(embed);
