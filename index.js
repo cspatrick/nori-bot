9a685a1ffa6ea1751abd8658e5e58169e2e11873
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-02-06 18:15:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const client = new Discord.Client();
 
 const prefix = '+';
 const ownerID = '275814889430188032';
-let status = 'my dick fall off';
-//let status = 'with Fatrick 💕';
+let status = '+help';
+//let status = 'with Patrick 💕';
 
 client.on('ready', () =>{
 
   
-    client.user.setPresence({game: {name: status , type: 3}, status: 'online'});
+    client.user.setPresence({game: {name: status , type: 0}, status: 'online'});
 
     //client.user.setPresence({activity: {name: status }, status: 'online'});
 
