1c08c96a26d3ffde92b0d9dbfca353b5ce679dd7
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-02-06 16:29:16 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '+';
+const ownerID = '275814889430188032';
+let status = 'my dick fall off';
+//let status = 'with Fatrick 💕';
+
+client.on('ready', () =>{
+
+  
+    client.user.setPresence({game: {name: status , type: 3}, status: 'online'});
+
+    //client.user.setPresence({activity: {name: status }, status: 'online'});
+
+});
+
+client.on('message', async message => {
+
+	let args = message.content.slice(prefix.length).trim().split(' ');
+	let cmd = args.shift().toLowerCase();
+
+  if(message.content.includes("XD") ){
+  message.delete({timeout: 1000});
+  return;
+  
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+}
+
+	try {
+		delete require.cache[require.resolve(`./commands/${cmd}.js`)];
+
+    let ops = {
+      ownerID: ownerID
+    }
+
+		let commandFile = require(`./commands/${cmd}.js`);
+		commandFile.run(client, message, args, ops);
+
+	} catch (e) {
+		console.log(e.stack);
+	}
+
+});
+
+
+client.on('ready', () => console.log('Launched!'));
+
+
+client.login('NTI2MDY0NzkwODA0ODg5NjUw.Dv_3_Q.lwoV61as7D4GlsI42SndlJqatSs');
