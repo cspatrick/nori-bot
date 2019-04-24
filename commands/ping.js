66e7a5dc02dde274204763ca1873bf02c24a8a36
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-04-23 19:05:18 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -9,7 +9,7 @@ exports.run = (client, message, args) => {
   message.delete({timeout: 1000});
 	
   setTimeout(() => {
-  message.channel.send('15 sec left to react or you will be removed from voice chat! Watch out for NOTS and dont spam options');
+  message.channel.send('15 sec left to react or you will be removed from voice chat! \n Watch out for NOTS and dont spam options.');
 	}, 15000);
 	
 }
