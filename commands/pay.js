238bab855eb21c78a0baa50dfb9b3c3104a42e07
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-04-24 21:02:22 -0700

    Create pay.js

---
+++ commands/pay.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args, ops) => {
+
+try{
+  if(!args[0]) return message.channel.send('Proper Usage: <prefix>pay text');
+
+  const embed = new Discord.RichEmbed()
+    .setColor(16754843)
+    .setFooter(`by ${message.author.username}`)
+    .addField('Pay', args.join(' ') )
+    .addField('Did you win?)
+    //.setDescription(args.join(' '))
+
+  let msg = await message.channel.send(embed);
+  
+  message.delete({timeout: 1000});
+  
+  await msg.react('👍');
+  await msg.react('👎');
+  await msg.react('🤷');
+
+
+ } catch (e){
+   console.log(e);
+ }
+
+}
