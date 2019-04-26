17afd98727dc8866a136f3aa87b5f79d8a34eeeb
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-04-26 15:40:50 -0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,8 +3,10 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args, ops) => {
 
 try{
-  if(!args[0]) return message.channel.send('Proper Usage: +poll question');
-
+  if(!args[0] && message.channel.id != '570803215310782480'){
+  return message.channel.send('Proper Usage: +poll question');
+  }
+  
   const embed = new Discord.RichEmbed()
     .setColor(16754843)
     .setFooter(`by ${message.author.username}`)
@@ -14,7 +16,6 @@ try{
     //.setTitle(`Poll created by ${message.author.username}`)
 
 
-  if (message.channel.id != '570803215310782480'){
   let msg = await message.channel.send(embed);
   
   message.delete({timeout: 1000});
@@ -22,7 +23,6 @@ try{
   await msg.react('👍');
   await msg.react('👎');
   await msg.react('🤷');
-  }
   
  } catch (e){
    console.log(e);
