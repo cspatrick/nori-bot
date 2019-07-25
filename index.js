14126a8a09f303cf572c2e43f303dc7983055002
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-07-25 01:16:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,8 +32,9 @@ client.on('message', async message => {
     .setColor(16754843)
     .setTitle('nots!');
 	
-  if((message.content.includes("(") || message.content.includes(")")) && !message.member.roles.equals("503792240569876511") 
-     && message.channel.id === '503351506205016105' && message.author.id == '160755148250021889'){
+  if((message.content.includes("(") || message.content.includes(")")) || message.content.includes("<:parenthesis:602010917450022922>")) 
+	  && !message.member.roles.equals("503792240569876511") 
+     && message.channel.id === '503351506205016105' && message.author.id == '191787373292421120'){
   message.delete(2000);
   message.channel.send("shut up");
   return;
