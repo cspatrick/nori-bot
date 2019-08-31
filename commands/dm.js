12a4b5f036439defeb6e5c01341bc254a20b84b4
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-08-31 16:50:14 -0700

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,13 +1,14 @@
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, ops) => {
-
+  
+mention = message.mentions.users.first();
+  
 try{
   if(!args[0] || mention == null){
   return message.channel.send('Proper Usage: +dm user message');
   }
-  
-  mention = message.mentions.users.first();
+
   mention.sendMessage(message.content());
   
  } catch (e){
