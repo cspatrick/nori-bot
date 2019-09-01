de6a32e2dc9cef3da8918ccde61a9b0a19f81eec
cspatrick <33044364+cspatrick@users.noreply.github.com>
2019-08-31 18:03:22 -0700

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -9,11 +9,6 @@ exports.run = async (client, message, args, ops) => {
       return message.channel.send('Proper Usage: +dm user message');
     }
     
-  if(message.author.id == '542873293246693413' && message.content.includes("Proper Usage")){
-     message.delete(5000);
-     return;
-     }
-    
   message.delete();
     
   if(message.member.roles.has('480870558720851998')){
